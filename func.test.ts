e73ec12dcea6c07920189b5ab54cd330b2e7e687
Sebastian Lindgren <sebastian.tb.lindgren@gmail.com>
2024-05-02 02:03:06 +0300

    Example start

--- func.test.ts
+++ func.test.ts
@@ -1,5 +1,44 @@
-import {sum} from "./func";
+import {Calculator} from "./func";
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('Adding 2+3 should result in 5', () => {
+  const calc = new Calculator([]);
+  calc.input(3);
+  calc.input(2);
+  calc.add();
+  expect(calc.latest()).toBe(5);
 });
+
+test('Subtracting 5-3 should result in 2', () => {
+  const calc = new Calculator([]);
+  calc.input(3);
+  calc.input(5);
+  calc.subtract();
+  expect(calc.latest()).toBe(2);
+});
+
+test('Multiplying 3*2 should result in 6', () => {
+  const calc = new Calculator([]);
+  calc.input(2);
+  calc.input(3);
+  calc.multiply();
+  expect(calc.latest()).toBe(6);
+});
+
+test('Dividing 9/3 should result in 3', () => {
+  const calc = new Calculator([]);
+  calc.input(3);
+  calc.input(9);
+  calc.divide();
+  expect(calc.latest()).toBe(3);
+});
+
+test('Printing all values in calculator', () => {
+  const calc = new Calculator([]);
+  calc.input(2);
+  calc.input(3);
+  calc.input(4);
+  expect(calc.all()).toStrictEqual([2,3,4]);
+});
+
+//TODO: Add some tests for more edge cases
+//TODO: Add some tests for some other functionality that you invent
--- func.ts
+++ func.ts
@@ -1,3 +1,62 @@
-export function sum(a, b) {
-    return a + b;
+//TODO: Think about how to solve the calculator (first version), build a working version.
+//TODO: Test to better the solution. Test a lot so that you know that it still works!
+
+export class Calculator {
+
+  inputs: number[];
+
+  constructor(initialNumbers){
+    this.inputs = initialNumbers;
   }
+
+  // Adds latest numbers
+  // Removes them from inputs-array
+  // Push new value to array
+  add(){
+    const num1 = this.inputs.pop();
+    const num2 = this.inputs.pop();
+
+    if(num1 && num2)
+      this.inputs.push(num1+num2);
+  }
+
+  // Multiplies latest numbers
+  // Removes them from inputs-array
+  // Push new value to array
+  multiply(){
+
+  }
+
+  // Subtract latest numbers 
+  // Removes them from inputs-array
+  // Push new value to array
+  subtract(){
+
+  }
+
+  // Divide two latest numbers
+  // Removes them from inputs-array
+  // Push new value to array
+  divide(){
+
+  }
+
+  // Add a number to the numers-array
+  input(input: number){
+    return this.inputs.push(input);
+  }
+
+  // Return latest response
+  latest(){
+    return this.peek();
+  }
+
+  // Return all numbers as array
+  all(){
+    
+  }
+
+  private peek() {
+    return this.inputs[this.inputs.length-1];
+  }
+}
